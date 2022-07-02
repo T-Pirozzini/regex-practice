5f4145eaa2c42c63ed164853375b6920cea26f9d
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-07-01 17:05:38 -0700

    implement validation function

---
+++ validation.js
@@ -0,0 +1,25 @@
+const inputs = document.querySelectorAll('input');
+console.log(inputs)
+
+const patterns = {
+  username: /^[a-z\d]{5,12}$/i,
+  password: /^[\w@-]{6,20}$/i,
+  telephone: /^\d{10}$/,
+  nickname: /^[a-z\d-]{8,20}$/
+}
+
+// validation function
+const validate = (field, regex) => {
+  if (regex.test(field.value)) {
+    field.className = 'valid'
+  } else {
+    field.className = 'invalid'
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener('keyup', (e) => {
+    validate(e.target, patterns[e.target.attributes.name.value])    
+  })
+})
+
